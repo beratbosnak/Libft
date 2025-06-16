aab9e8ec9d3b1db1ee9fa690474fe4013d453dfe
Berat Boşnak <bbosnak@Berats-Mac-mini.local>
2025-06-16 11:44:12 -0700

    Sync repo with local changes

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbosnak <bbosnak@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/26 18:39:42 by bbosnak           #+#    #+#             */
+/*   Updated: 2022/12/26 18:39:43 by bbosnak          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	tot_size;
+	void	*dst;
+
+	tot_size = size * count;
+	dst = malloc(tot_size);
+	if (!dst)
+		return (0);
+	ft_memset(dst, 0, tot_size);
+	return (dst);
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbosnak <bbosnak@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/27 19:17:37 by bbosnak           #+#    #+#             */
+/*   Updated: 2022/12/27 19:17:38 by bbosnak          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*array;
+	int		i;
+
+	if (!s)
+		return (0);
+	array = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!array)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		array[i] = f(i, s[i]);
+		i++;
+	}
+	array[i] = '\0';
+	return (array);
+}
