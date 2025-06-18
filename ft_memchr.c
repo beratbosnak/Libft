6bf25a9f0f0d29c587fc42478abe0a1a92249621
Berat Boşnak <110120659+beratbosnak@users.noreply.github.com>
2025-06-18 04:58:00 -0700

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,13 +14,17 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*ptr;
+	unsigned char	target;
 
 	i = 0;
+	ptr = (unsigned char *)s;
+	target = (unsigned char)c;
 	while (i < n)
 	{
-		if (((char *)s)[i] == c)
-			return ((void *)&s[i]);
+		if (ptr[i] == target)
+			return ((void *)&ptr[i]);
 		i++;
 	}
 	return (NULL);
