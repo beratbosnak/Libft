e40ecfb84d28335b6590081889649e97186c0801
Berat Boşnak <110120659+beratbosnak@users.noreply.github.com>
2025-06-18 08:20:44 -0700

    Correction of incorrect function

--- ft_calloc.c
+++ ft_calloc.c
@@ -10,10 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(int c)
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_calloc(size_t count, size_t size)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-		return (1);
-	else
-		return (0);
+	void	*dst;
+
+	if (count == 0 || size == 0)
+		return (malloc(1));
+	if (count > 65535 || size > 65535)
+		return (NULL);
+	dst = malloc(count * size);
+	if (!dst)
+		return (NULL);
+	ft_memset(dst, 0, count * size);
+	return (dst);
 }
